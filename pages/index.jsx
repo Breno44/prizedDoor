34aa6d14b9e01cbef9b8e72db3808facf4dd149b
Breno <bhfs.2908@gmail.com>
2021-09-23 18:42:21 -0300

    feat:visual port component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
-import Gift from "../components/Gift";
+import Door from "../components/Door";
 
 export default function Home() {
   return (
     <div>
-      <Gift />
+      <Door />
     </div>
   );
 }
