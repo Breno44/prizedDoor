141b148f05bf02a4a1fda4cf9dd676f65009c0b1
Breno <bhfs.2908@gmail.com>
2021-09-24 16:53:03 -0300

    feat:finish door

--- components/Door.jsx
+++ components/Door.jsx
@@ -1,8 +1,12 @@
 import styles from "../styles/Door.module.css";
-export default function Door() {
+
+export default function Door(props) {
+
+  const select = props.selected ? styles.select : '';
+
   return (
     <div className={styles.area}>
-      <div className={styles.structure}>
+      <div className={`${styles.structure} ${select}`}>
         <div className={styles.door}>
           <div className={styles.number}>3</div>
           <div className={styles.doorHandle}></div>
