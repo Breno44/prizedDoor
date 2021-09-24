fd272decc3268cfc8c3b20342b894986f74b920a
Breno <bhfs.2908@gmail.com>
2021-09-24 18:50:21 -0300

    feat:finish door component

--- components/Door.tsx
+++ components/Door.tsx
@@ -8,18 +8,26 @@ interface DoorProps {
 
 export default function Door(props: DoorProps) {
   const door = props.value;
-  const select = door.selected ? styles.select : "";
+  const select = door.selected && !door.open ? styles.select : "";
 
   const toogleSelection = (e) => props.onChange(door.toggleSelection());
+  const open = (e) => {
+    e.stopPropagation();
+    props.onChange(door.openDoor());
+  };
+
+  function renderDoor() {
+    return (
+      <div className={styles.door}>
+        <div className={styles.number}>{door.number}</div>
+        <div className={styles.doorHandle} onClick={open}></div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.area} onClick={toogleSelection}>
-      <div className={`${styles.structure} ${select}`}>
-        <div className={styles.door}>
-          <div className={styles.number}>{door.number}</div>
-          <div className={styles.doorHandle}></div>
-        </div>
-      </div>
+      <div className={`${styles.structure} ${select}`}>{door.open ? false : renderDoor()}</div>
       <div className={styles.floor}></div>
     </div>
   );
