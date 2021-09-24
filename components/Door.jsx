f9c42d4743d48ae377e9bf7b95460b67d9ed7c3b
Breno <bhfs.2908@gmail.com>
2021-09-24 14:53:38 -0300

    feat:door frame

--- components/Door.jsx
+++ components/Door.jsx
@@ -1,8 +1,12 @@
 import styles from "../styles/Door.module.css";
 export default function Door() {
   return (
-    <div className={styles.door}>
-      <div className={styles.number}>3</div>
+    <div className={styles.area}>
+      <div className={styles.structure}>
+        <div className={styles.door}>
+          <div className={styles.number}>3</div>
+        </div>
+      </div>
     </div>
   );
 }
