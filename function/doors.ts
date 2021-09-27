6d10fbcac989e7e4b38464d3d167aedf45319c78
Breno <bhfs.2908@gmail.com>
2021-09-27 14:57:43 -0300

    feat:finish func create doors

--- function/doors.ts
+++ function/doors.ts
@@ -8,3 +8,15 @@ export function createDoors(qtde: number, selected: number):DoorModel[] {
         return new DoorModel(number, haveGift)
     })
 }
+
+export function updateDoors(doors: DoorModel[], modifiedDoor: DoorModel): DoorModel[] {
+    return doors.map(door => {
+        const modified = door.number === modifiedDoor.number
+
+        if(modified) {
+            return modifiedDoor
+        } else {
+            return modifiedDoor.open ? door : door.deselect()
+        }
+    })
+}
