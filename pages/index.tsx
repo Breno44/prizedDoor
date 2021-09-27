79c96a2555581e7f98aa2eddc9d6177b53c65e3c
Breno <bhfs.2908@gmail.com>
2021-09-27 16:52:00 -0300

    feat:add form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import styles from "../styles/Form.module.css";
+import Card from "../components/Card";
+import Link from "next/link";
+
+export default function Form() {
   return (
-    <div style={{ display: "flex" }}>
-      <h1>Formulario de inicio!!!</h1>
+    <div className={styles.form}>
+      <div>
+        <Card bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card></Card>
+      </div>
+      <div>
+        <Card></Card>
+        <Card bgcolor="#28a085">
+          <Link href={`/game/4/2`}>
+            <h2 className={styles.link} style={{ flex: "1", margin: "0" }}>
+              Iniciar
+            </h2>
+          </Link>
+        </Card>
+      </div>
     </div>
   );
 }
